a99d8eada4356d572b3694944d68dff87663344a
Biozard01 <arthur.laforest33@gmail.com>
2021-05-21 11:13:45 +0200

    req start + commit bdd

---
+++ Req.sql
@@ -0,0 +1,17 @@
+USE BourgeMiel
+
+SELECT * FROM "Biomes"
+
+SELECT * FROM "Drop"
+
+SELECT * FROM "Equipments"
+
+SELECT * FROM "Items"
+
+SELECT * FROM "Monsters"
+
+SELECT * FROM "Players"
+
+SELECT * FROM "Rarities"
+
+SELECT * FROM "Zones"
