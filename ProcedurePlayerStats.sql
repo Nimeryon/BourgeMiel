da402914e0e8704075f3d085960f02108d3603f5
Nimeryon <t.auriol46@gmail.com>
2021-05-27 14:44:37 +0200

    procedure

--- ProcedurePlayerStats.sql
+++ ProcedurePlayerStats.sql
@@ -2,47 +2,16 @@ GO
     USE BourgeMiel
 GO
 
-CREATE OR ALTER FUNCTION PlayerHealth(@PlayerID INT)
-RETURNS INT
+CREATE OR ALTER PROCEDURE PlayerStats(@PlayerID INT)
 AS
 BEGIN
-	DECLARE @Health INT
+	DECLARE @Health INT, @Armor INT, @Attack INT
 
-	SELECT @Health = p.PlayerHealth + SUM(i.ItemHealthStat) FROM Players as p
+	SELECT @Health = p.PlayerHealth + SUM(i.ItemHealthStat), @Armor = p.PlayerArmor + SUM(i.ItemArmorStat), @Attack = p.PlayerDamage + SUM(i.ItemDamageStat) FROM Players as p
 	INNER JOIN Equipments as e ON e.PlayerId = p.PlayerId
 	INNER JOIN Items as i ON i.ItemId = e.ItemId
 	WHERE p.PlayerId = @PlayerID
 	GROUP BY p.PlayerHealth, p.PlayerArmor, p.PlayerDamage
 
-    RETURN @Health;
-END;
-
-CREATE OR ALTER FUNCTION PlayerArmor(@PlayerID INT)
-RETURNS INT
-AS
-BEGIN
-	DECLARE @Armor INT
-
-	SELECT @Armor = p.PlayerArmor + SUM(i.ItemArmorStat) FROM Players as p
-	INNER JOIN Equipments as e ON e.PlayerId = p.PlayerId
-	INNER JOIN Items as i ON i.ItemId = e.ItemId
-	WHERE p.PlayerId = @PlayerID
-	GROUP BY p.PlayerArmor
-
-    RETURN @Armor;
-END;
-
-CREATE OR ALTER FUNCTION PlayerAttack(@PlayerID INT)
-RETURNS INT
-AS
-BEGIN
-	DECLARE @Attack INT
-
-	SELECT @Attack = p.PlayerDamage + SUM(i.ItemDamageStat) FROM Players as p
-	INNER JOIN Equipments as e ON e.PlayerId = p.PlayerId
-	INNER JOIN Items as i ON i.ItemId = e.ItemId
-	WHERE p.PlayerId = @PlayerID
-	GROUP BY p.PlayerDamage
-
-    RETURN @Attack;
+	PRINT CONCAT('Health: ', @Health, ' / Armor: ', @Armor, ' / Attack: ', @Attack)
 END;
