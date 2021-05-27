388adc252aceaede58defa739f14729061f04608
Nimeryon <t.auriol46@gmail.com>
2021-05-27 14:46:31 +0200

    procedure

--- ProcedurePlayerStats.sql
+++ ProcedurePlayerStats.sql
@@ -7,11 +7,9 @@ AS
 BEGIN
 	DECLARE @Health INT, @Armor INT, @Attack INT
 
-	SELECT @Health = p.PlayerHealth + SUM(i.ItemHealthStat), @Armor = p.PlayerArmor + SUM(i.ItemArmorStat), @Attack = p.PlayerDamage + SUM(i.ItemDamageStat) FROM Players as p
-	INNER JOIN Equipments as e ON e.PlayerId = p.PlayerId
-	INNER JOIN Items as i ON i.ItemId = e.ItemId
-	WHERE p.PlayerId = @PlayerID
-	GROUP BY p.PlayerHealth, p.PlayerArmor, p.PlayerDamage
+	SET @Health = dbo.PlayerHealth(@PlayerID)
+	SET @Armor = dbo.PlayerArmor(@PlayerID)
+	SET @Attack = dbo.PlayerAttack(@PlayerID)
 
 	PRINT CONCAT('Health: ', @Health, ' / Armor: ', @Armor, ' / Attack: ', @Attack)
 END;
