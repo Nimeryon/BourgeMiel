b288b844ff2067149c260d39b26fc4932da02d65
Biozard01 <arthur.laforest33@gmail.com>
2021-05-27 13:10:04 +0200

    trigger 1

--- Req.sql
+++ Req.sql
@@ -2,10 +2,16 @@ GO
 	USE BourgeMiel
 GO
 
-SELECT p.PlayerLevel, p.PlayerName,  p.PlayerHealth, (p.PlayerHealth - dbo.DamageCalc(p.PlayerArmor, m.MonsterDamage)) AS 'Vie restante au joueur', (m.MonsterHealth - dbo.DamageCalc(m.MonsterArmor, p.PlayerDamage)) AS 'Vie restante au monstre'
+/*SELECT p.PlayerLevel, p.PlayerName,  p.PlayerHealth, (p.PlayerHealth - dbo.DamageCalc(p.PlayerArmor, m.MonsterDamage)) AS 'Vie restante au joueur', (m.MonsterHealth - dbo.DamageCalc(m.MonsterArmor, p.PlayerDamage)) AS 'Vie restante au monstre'
 FROM "Players" p 
 INNER JOIN "Equipments" e ON p.PlayerId = e.PlayerId 
 INNER JOIN Items i ON e.ItemId = i.ItemId 
 INNER JOIN "Drops" d ON i.ItemId = d.ItemId	
 INNER JOIN "Monsters" m	ON d.MonsterId = m.MonsterId
-ORDER BY p.PlayerLevel DESC
+ORDER BY p.PlayerLevel DESC*/
+
+SELECT * FROM Players WHERE PlayerId = 45
+
+UPDATE Players SET
+PlayerLevel = PlayerLevel + 1
+WHERE PlayerId = 45 AND PlayerLevel != 100
