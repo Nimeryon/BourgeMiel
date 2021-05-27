2f339d2cb4694628db3bcb735e819583faa37207
Biozard01 <arthur.laforest33@gmail.com>
2021-05-27 09:58:07 +0200

    funct 1

--- Req.sql
+++ Req.sql
@@ -2,12 +2,20 @@ GO
 	USE BourgeMiel
 GO
 
-SELECT dbo.FightWinner(p.PlayerHealth, p.PlayerArmor, p.PlayerDamage, m.MonsterHealth, m.MonsterArmor, m.MonsterDamage) AS 'Winner' FROM "Players" p
-INNER JOIN "Equipments" e
-ON(p.PlayerId = e.PlayerId)
-INNER JOIN Items i
-ON (e.ItemId = i.ItemId)
-INNER JOIN "Drop" d
-ON (i.ItemId = d.ItemId)
-INNER JOIN "Monsters" m
-ON (d.MonsterId = m.MonsterId)
+SELECT p.PlayerId, p.PlayerArmor, m.MonsterDamage, dbo.DamageCalc(p.PlayerArmor, m.MonsterDamage) AS 'True Damage'
+FROM "Players" p 
+INNER JOIN "Equipments" e ON p.PlayerId = e.PlayerId 
+INNER JOIN Items i ON e.ItemId = i.ItemId 
+INNER JOIN "Drops" d ON i.ItemId = d.ItemId	
+INNER JOIN "Monsters" m	ON d.MonsterId = m.MonsterId
+WHERE p.PlayerId = 1
+ORDER BY p.PlayerId ASC
+
+
+SELECT p.PlayerArmor, m.MonsterArmor, m.MonsterName
+FROM "Players" p 
+INNER JOIN "Equipments" e ON p.PlayerId = e.PlayerId 
+INNER JOIN Items i ON e.ItemId = i.ItemId 
+INNER JOIN "Drops" d ON i.ItemId = d.ItemId	
+INNER JOIN "Monsters" m	ON d.MonsterId = m.MonsterId
+ORDER BY m.MonsterArmor DESC
